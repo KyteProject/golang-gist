7ca896196cf743c9bd523f41bd37fa3fbd67dbca
Daniel Smith <headhertz88@gmail.com>
2019-10-16 18:50:05 +0100

    some tests

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -10,7 +10,11 @@ import (
 )
 
 func humanDate(t time.Time) string {
-	return t.Format("02 Jan 2006 at 15:04")
+	if t.IsZero() {
+		return ""
+	}
+
+	return t.UTC().Format("02 Jan 2006 at 15:04")
 }
 
 var functions = template.FuncMap{
