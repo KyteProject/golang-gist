2f863d846aadbdb6a47593b4c46c55463723d3bf
Daniel Smith <headhertz88@gmail.com>
2019-10-17 10:20:14 +0100

    integration tests

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"html"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
+	"regexp"
 	"testing"
 	"time"
 
@@ -13,28 +16,34 @@ import (
 	"github.com/kyteproject/golang-gist/pkg/models/mock"
 )
 
-func newTestApplication(t *testing.T) *application {
-	templateCache, err := newTemplateCache("./../../ui/html/")
-	if err != nil {
-		t.Fatal(err)
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body []byte) string {
+	matches := csrfTokenRX.FindSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
 	}
 
-	session := sessions.New([]byte("3dSm5MnygFHh7XidAtbskXrjbwfoJcbJ"))
-	session.Lifetime = 12 * time.Hour
-	session.Secure = true
-
-	return &application{
-		errorLog:      log.New(ioutil.Discard, "", 0),
-		infoLog:       log.New(ioutil.Discard, "", 0),
-		session:       session,
-		snippets:      &mock.SnippetModel{},
-		templateCache: templateCache,
-		users:         &mock.UserModel{},
-	}
+	return html.UnescapeString(string(matches[1]))
 }
 
 type testServer struct {
 	*httptest.Server
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, []byte) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer rs.Body.Close()
+	body, err := ioutil.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return rs.StatusCode, rs.Header, body
 }
 
 func newTestServer(t *testing.T, h http.Handler) *testServer {
@@ -67,4 +76,24 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byt
 	}
 
 	return rs.StatusCode, rs.Header, body
+}
+
+func newTestApplication(t *testing.T) *application {
+	templateCache, err := newTemplateCache("./../../ui/html/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	session := sessions.New([]byte("3dSm5MnygFHh7XidAtbskXrjbwfoJcbJ"))
+	session.Lifetime = 12 * time.Hour
+	session.Secure = true
+
+	return &application{
+		errorLog:      log.New(ioutil.Discard, "", 0),
+		infoLog:       log.New(ioutil.Discard, "", 0),
+		session:       session,
+		snippets:      &mock.SnippetModel{},
+		templateCache: templateCache,
+		users:         &mock.UserModel{},
+	}
 }
---
+++ pkg/models/mysql/testutils_test.go
@@ -0,0 +1,36 @@
+package mysql
+
+import (
+	"database/sql"
+	"io/ioutil"
+	"testing"
+)
+
+func newTestDB(t *testing.T) (*sql.DB, func()) {
+	db, err := sql.Open("mysql", testDsn)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	script, err := ioutil.ReadFile("./testdata/setup.sql")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = db.Exec(string(script))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return db, func() {
+		script, err := ioutil.ReadFile("./testdata/teardown.sql")
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = db.Exec(string(script))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		db.Close()
+	}
+}
